90d3b1aa90b4e9bf1dc30ec5c59497e06d84629e
jro <j@ro.vu>
2020-04-09 10:26:05 -0500

    Initial

--- tts.sh
+++ tts.sh
@@ -13,7 +13,15 @@ echo "Using $input as input."
 
 while IFS= read -r line
 do
-#	echo "$line"
+	echo "Processing: $line"
+
+	# aws polly synthesize-speech \ 
+	#    --output-format mp3 \
+	#    --voice-id Joanna \
+	#    --text 'Hello, my name is Joanna. I learned about the W3C on 10/3 of last year.' \
+	#    hello.mp3
+
+
 
 	filename="$output$line"
 	touch "${filename}"
