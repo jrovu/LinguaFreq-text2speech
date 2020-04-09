324195a094c69c602f3bae6443addb66fef6bcef
jro <j@ro.vu>
2020-04-09 10:45:33 -0500

    Minor refactoring / renaming

--- tts.sh
+++ tts.sh
@@ -2,48 +2,46 @@
 
 echo "Text to Speech using AWS Polly"
 
-input="_Input/sentences.txt"
-output="_Output/"
+# TODO: Make these configurable
+input_dir="_Input/"
+input_file="sentences.txt"
+output_dir="_Output/"
+silence_file="_Resources/1SecSilence.mp3"
 
-echo "Using $input as input."
+echo "Using ${input_dir}${input_file} as input."
 
-# Open a text file
 
-# Loop over each line
-
+# Loop over each line of input file
 while IFS= read -r line
 do
-	echo "Processing: $line"
-	filename="$output$line"
 
+	echo "Processing: $line"
+	
+	# Name the file based off the text
+	filename="${line}.mp3"
+
+	# Create MP3s using AWS Polly
 	aws polly synthesize-speech \
 	   --output-format mp3 \
 	   --voice-id Lupe \
 	   --text "${line}" \
-	   "${filename}".mp3
-
-
-
+	   "${output_dir}${filename}"
 
 	
-
-done < "$input"
-
+	#ffmpeg -i "concat:${output}${filename}|${silence_file}" -c copy 
 
 
 
-# Read in each line
+done < "${input_dir}${input_file}"
 
-# Pass the line as a text to speech, and also the output file name
-
-# Save as a file
 
 # Add gap
 
 
 
 
-
+# Voice list: https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
+# Polly Console: https://console.aws.amazon.com/polly/home/SynthesizeSpeech
 
 
 #aws polly synthesize-speech \ 
